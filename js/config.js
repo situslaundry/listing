bcb7aeefaf42d4880449b5bd15916ac1f778c271
situslaundry <situslaundry@gmail.com>
2026-08-14 19:42:10 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,6 @@
-// js/config.js
 window.SITE_CONFIG = {
   siteName: "Laundry Listing",
   defaultPhone: "6285773009666",
-  baseDomain: "https://situslaundry.github.io", // Ganti jika menggunakan custom domain lain
   defaultPricePerKg: "Rp 7.000",
   expressPricePerKg: "Rp 12.000",
   cuciSetrikaPricePerKg: "Rp 10.000"
