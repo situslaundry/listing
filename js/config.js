5a0628fd4a8e0f4ce19a4e46f3e39f8d81ccc6b4
situslaundry <situslaundry@gmail.com>
2026-08-15 19:09:56 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,3 @@
 window.SITE_CONFIG = {
-  siteName: "Laundry Listing",
-  defaultPhone: "6285773009666",
-  defaultPricePerKg: "Rp 7.000",
-  expressPricePerKg: "Rp 12.000",
-  cuciSetrikaPricePerKg: "Rp 10.000"
+  defaultPhone: "6285773009666"
 };
